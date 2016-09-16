dc3ab3877b6ba76dcca0d1fcddc4bfc03ca09bf2
Melanie27 <melaniemcganney@gmail.com>
2016-09-16 10:43:11 -0700

    bloc maps initial commit

---
+++ BlocSpot/BlocSpot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocSpot
+//
+//  Created by MELANIE MCGANNEY on 9/16/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
