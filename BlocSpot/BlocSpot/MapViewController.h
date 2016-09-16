eaffb888ee3cac97235043362c4635fe614bde82
Melanie27 <melaniemcganney@gmail.com>
2016-09-16 10:48:11 -0700

    feature/query-maps added title and VC

---
+++ BlocSpot/BlocSpot/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  BlocSpot
+//
+//  Created by MELANIE MCGANNEY on 9/16/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ BlocSpot/BlocSpot/MapViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewController.m
+//  BlocSpot
+//
+//  Created by MELANIE MCGANNEY on 9/16/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.navigationItem.title = @"BlocSpot Map";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
