4dd2072d416ce4f31d3365b64d6971cc50ceb85d
Melanie27 <melaniemcganney@gmail.com>
2016-09-20 09:38:23 -0700

    feature/query-maps model objects

---
+++ BlocSpot/BlocSpot/Search.h
@@ -0,0 +1,13 @@
+//
+//  Search.h
+//  BlocSpot
+//
+//  Created by MELANIE MCGANNEY on 9/20/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Search : NSObject
+
+@end
