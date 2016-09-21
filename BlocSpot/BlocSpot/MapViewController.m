f0d3195ac379ce1227ec349e131b72d7eef4d98a
Melanie27 <melaniemcganney@gmail.com>
2016-09-20 18:47:04 -0700

    custom annotation class

--- BlocSpot/BlocSpot/MapViewController.m
+++ BlocSpot/BlocSpot/MapViewController.m
@@ -43,11 +43,50 @@ CLLocationManager *locationManager;
     }
     
     
+    //Add some annotations
+    MKPointAnnotation *annotation1 = [[MKPointAnnotation alloc]init];
+    annotation1.title = @"Santa Barbara";
+    annotation1.subtitle = @"Vacation spot";
+    annotation1.coordinate = CLLocationCoordinate2DMake(34.4208, -119.6982);
+    
+    MKPointAnnotation *annotation2 = [[MKPointAnnotation alloc]init];
+    annotation2.title = @"Palm Springs";
+    annotation2.subtitle = @"It's like a sauna";
+    annotation2.coordinate = CLLocationCoordinate2DMake(33.8303, -116.5453);
+    
+    [self.mapView addAnnotation:annotation1];
+    [self.mapView addAnnotation:annotation2];
     
 }
 
--(void)startUserTracking {
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
     
+    
+    //don't create annotation views for the user location
+    if([annotation isKindOfClass:[MKPointAnnotation class]]) {
+        
+        static NSString *userPinAnnotationPurpleId = @"userPinAnnotation";
+        
+        //create an annotation view, but reuse a cached on if available
+        MKPinAnnotationView *annotationView = (MKPinAnnotationView *)[self.mapView dequeueReusableAnnotationViewWithIdentifier:(userPinAnnotationPurpleId)];
+        
+        if(annotationView) {
+            //cached view found, set pin color only
+            annotationView.annotation = annotation;
+            
+        } else {
+            //no cached views available
+            annotationView = [[MKPinAnnotationView alloc] initWithAnnotation:(annotation) reuseIdentifier:userPinAnnotationPurpleId];
+            
+            //purple indicates user defined pin
+            annotationView.pinTintColor = [UIColor purpleColor];
+        }
+        
+        return annotationView;
+    
+    }
+    //returning nil results in default annotation mark
+    return nil;
 }
 
 -(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
