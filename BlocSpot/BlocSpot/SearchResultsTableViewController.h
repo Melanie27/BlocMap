87955deebad1f9865a7556fb5fa0039e6e6ec328
Melanie27 <melaniemcganney@gmail.com>
2016-09-22 11:48:05 -0700

    feature/query-maps view controllers set up now

---
+++ BlocSpot/BlocSpot/SearchResultsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchResultsTableViewController.h
+//  BlocSpot
+//
+//  Created by MELANIE MCGANNEY on 9/22/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchResultsTableViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
