775f938a2a91ce809e29ec146a7db032d320a97d
Melanie27 <melaniemcganney@gmail.com>
2016-10-03 13:19:36 -0700

    feature/create-categories delete old vc

--- BlocSpot/BlocSpot/SearchResultsTableViewController.m
+++ BlocSpot/BlocSpot/SearchResultsTableViewController.m
@@ -129,7 +129,7 @@
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if([segue.identifier isEqualToString:@"resultDetail"]) {
-        ResultsDetailViewController *resultDetailVC = (ResultsDetailViewController*)segue.destinationViewController;
+        //ResultsDetailViewController *resultDetailVC = (ResultsDetailViewController*)segue.destinationViewController;
        
         
     }
